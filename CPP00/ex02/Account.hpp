3a12566387ef9ede13b3e3e620f8686ca7019a6a
Olympe <67268136+opacaud@users.noreply.github.com>
2022-03-23 20:32:02 +0100

    Update Account.hpp

--- CPP00/ex02/Account.hpp
+++ CPP00/ex02/Account.hpp
@@ -12,15 +12,15 @@
 
 # define ACCOUNT_HPP
 
-# define RESET  		"\033[0m"
-# define BLACK  		"\033[30m"			/* Black */
-# define RED    		"\033[31m"			/* Red */
-# define GREEN  		"\033[32m"			/* Green */
-# define YELLOW 		"\033[33m"			/* Yellow */
-# define BLUE   		"\033[34m"			/* Blue */
-# define MAGENTA		"\033[35m"			/* Magenta */
-# define CYAN   		"\033[36m"			/* Cyan */
-# define WHITE  		"\033[37m"			/* White */
+# define RESET  	"\033[0m"
+# define BLACK  	"\033[30m"		/* Black */
+# define RED    	"\033[31m"		/* Red */
+# define GREEN  	"\033[32m"		/* Green */
+# define YELLOW 	"\033[33m"		/* Yellow */
+# define BLUE   	"\033[34m"		/* Blue */
+# define MAGENTA	"\033[35m"		/* Magenta */
+# define CYAN   	"\033[36m"		/* Cyan */
+# define WHITE  	"\033[37m"		/* White */
 # define BOLDBLACK  	"\033[1m\033[30m"	/* Bold Black */
 # define BOLDRED    	"\033[1m\033[31m"	/* Bold Red */
 # define BOLDGREEN  	"\033[1m\033[32m"	/* Bold Green */
@@ -38,7 +38,7 @@ class Account
 {
 	public:
 
-		typedef Account		t;
+		typedef Account	t;
 
 		Account( int initial_deposit );
 		~Account( void );
@@ -51,7 +51,7 @@ class Account
 
 		void		makeDeposit( int deposit );
 		bool		makeWithdrawal( int withdrawal );
-		int			checkAmount( void ) const;
+		int		checkAmount( void ) const;
 		void		displayStatus( void ) const;
 
 	private:
@@ -63,10 +63,10 @@ class Account
 
 		static void	_displayTimestamp( void );
 
-		int			_accountIndex;
-		int			_amount;
-		int			_nbDeposits;
-		int			_nbWithdrawals;
+		int		_accountIndex;
+		int		_amount;
+		int		_nbDeposits;
+		int		_nbWithdrawals;
 
 		Account( void );
 };
