af74b710c7ce3f064099c3ac6878cf1e3c66087e
Olympe <67268136+opacaud@users.noreply.github.com>
2022-03-23 20:32:32 +0100

    Update tests.cpp

--- CPP00/ex02/tests.cpp
+++ CPP00/ex02/tests.cpp
@@ -15,9 +15,9 @@
 
 int		main( void ) {
 
-	typedef std::vector<Account::t>							  accounts_t;
-	typedef std::vector<int>								  ints_t;
-	typedef std::pair<accounts_t::iterator, ints_t::iterator> acc_int_t;
+	typedef std::vector<Account::t>					accounts_t;
+	typedef std::vector<int>					ints_t;
+	typedef std::pair<accounts_t::iterator, ints_t::iterator>	acc_int_t;
 
 	int	const				amounts[]	= { 42, 54, 957, 432, 1234, 0, 754, 16576 };
 	size_t const			amounts_size( sizeof(amounts) / sizeof(int) );
